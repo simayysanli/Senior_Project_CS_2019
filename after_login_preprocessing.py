6f330fdfc5961b970556a4cfe1559aaea76e0927
berksudan <berk.sudan.96@gmail.com>
2019-06-08 15:59:36 +0300

    Files merged and merged_file created.

--- after_login_preprocessing.py
+++ after_login_preprocessing.py
@@ -1,70 +1,76 @@
-# ######### PROCESS LIST ############################################################
-# CSV: CSV format of input log
-# SF: Given features are selected and others have been omitted.
-# Sorted: Data is sorted by 3 attribute respectively.
-# ExCol: Exchanged 2 given columns in csv file.
-# VDur: Extracted video durations in video views.
-# FiNones: If there is any absent item in csv file, fill with NONEs.
-# FilteredPV: Final summarized page views table with added session-durations, unique-link-counts,link-counts
-# ###################################################################################
+"""
+########################### List of Process Tags  ################################
+-> [CSV]: CSV format of input log
+-> [SF]: Given features are selected and others have been omitted.
+-> [Sorted]: Data is sorted by 3 attribute respectively.
+-> [ExCol]: Exchanged 2 given columns in csv file.
+-> [VDur]: Extracted video durations in video views.
+-> [FiNones]: If there is any absent item in csv file, fill with NONEs.
+-> [FilteredPV]: Final summarized page views table with added session-durations, link-counts, unique-link-counts.
+-> [FilteredVV]: Final summarized video views table with added video-durations, video-counts, unique-video-counts.
+-> [NoNulls]: Removed the rows that contain 'NULL' word.
+###################################################################################
+"""
+from preprocess_operations import PreProcessOperations as Ppo
 
 
-import preprocess_operations
-
-
-def create_file_names(repo_dir, ds_name, process_labels, extension):
-    file_names = []
-    cum_p_labels = ['[%s]' % process_labels[0]]  # Cumulative Process Labels
-    for i in range(1, len(process_labels)):
-        cum_p_labels.append('%s[%s]' % (cum_p_labels[i - 1], process_labels[i]))
-
-    for label in cum_p_labels:
-        file_names.append(repo_dir + ds_name + label + extension)
-
-    return file_names
-
-
-def process_page_views(repo_dir):
+def process_page_views(repo_dir, pv_data_set):
     page_views_processes = ['CSV', 'SF', 'Sorted', 'FilteredPV']
-    page_views_data_set = 'pageViews'  # name of data set
-    pv_file_names = create_file_names(repo_dir, page_views_data_set, page_views_processes, extension='.csv')
+    pv_file_names = Ppo.create_file_names_with_p_tags(repo_dir, pv_data_set, page_views_processes, out_extension='csv')
 
-    ppo_pv = preprocess_operations.PreProcessOperations()
+    Ppo.after_login_log_to_csv(pv_file_names[0])
+    Ppo.select_features_in_csv(pv_file_names[1], selected_features=[1, 2, 3, 4, 5])  # ID feature is omitted.
+    Ppo.sort_csv_by_header(pv_file_names[2], feature1='user_id', feature2='session_id', feature3='date',
+                           feature4='time')
+    Ppo.filter_page_views(pv_file_names[3], usr_idx=0, sid_idx=1, date_idx=2, time_idx=3, url_idx=4,
+                          time_format='%H:%M:%S.%f', date_format='%Y-%m-%d')
 
-    # ppo_pv.preprocessed_log_to_csv(repo_dir + page_views_data_set + '.txt')
-    # ppo_pv.select_features_in_csv(pv_file_names[0], selected_features=[1, 2, 3, 4, 5])  # ID feature is omitted.
-    # ppo_pv.sort_csv_by_header(pv_file_names[1], feature1='user_id', feature2='session_id', feature3='date',
-    # feature4 = 'time')
-    # ppo_pv.filter_page_views(pv_file_names[2], usr_idx=0, sid_idx=1, date_idx=2, time_idx=3, url_idx=4,
-    # time_format='%H:%M:%S.%f', date_format='%Y-%m-%d')
+    return pv_file_names[len(page_views_processes)]
 
 
-def process_video_views(repo_dir):
+def process_video_views(repo_dir, vv_data_set):
     video_views_processes = ['CSV', 'FiNones', 'SF', 'Sorted', 'ExCol', 'VDur', 'FilteredVV']
-    video_views_data_set = 'videoViews'  # name of data set
-    vv_file_names = create_file_names(repo_dir, video_views_data_set, video_views_processes, extension='.csv')
+    vv_file_names = Ppo.create_file_names_with_p_tags(repo_dir, vv_data_set, video_views_processes, out_extension='csv')
 
-    ppo_vv = preprocess_operations.PreProcessOperations()
+    Ppo.after_login_log_to_csv(vv_file_names[0])
+    Ppo.fill_with_nones(vv_file_names[1])
+    Ppo.select_features_in_csv(vv_file_names[2], selected_features=[1, 2, 3, 4, 5, 6, 7])  # ID feature is omitted.
+    Ppo.sort_csv_by_header(vv_file_names[3], feature1='user_id', feature2='session_id', feature3='date',
+                           feature4='time')
+    Ppo.exchange_columns(vv_file_names[4], feature1='user_id', feature2='session_id')
+    Ppo.extract_video_durations(vv_file_names[5], 'date', 'time', 'exit_date', 'exit_time',
+                                time_format='%H:%M:%S.%f', date_format='%Y-%m-%d')
+    Ppo.filter_video_views(vv_file_names[6], vid_id_idx=0, usr_idx=1, sid_idx=2, vid_dur_idx=5)
 
-    ppo_vv.preprocessed_log_to_csv(repo_dir + video_views_data_set + '.txt')
-    ppo_vv.fill_with_nones(vv_file_names[0])
-    ppo_vv.select_features_in_csv(vv_file_names[1], selected_features=[1, 2, 3, 4, 5, 6, 7])  # ID feature is omitted.
-    ppo_vv.sort_csv_by_header(vv_file_names[2], feature1='user_id', feature2='session_id', feature3='date',
-                              feature4='time')
-    ppo_vv.exchange_columns(vv_file_names[3], feature1='user_id', feature2='session_id')
-    ppo_vv.extract_video_durations(vv_file_names[4], 'date', 'time', 'exit_date', 'exit_time',
-                                   time_format='%H:%M:%S.%f', date_format='%Y-%m-%d')
-    ppo_vv.filter_video_views(vv_file_names[5], vid_id_idx=0, usr_idx=1, sid_idx=2, vid_dur_idx=5)
+    return vv_file_names[len(video_views_processes)]
+
+
+def process_merged_file(repo_dir, mf_data_set):
+    merged_file_processes = ['NoNulls', 'SF']
+    mf_file_names = Ppo.create_file_names_with_p_tags(repo_dir, mf_data_set, merged_file_processes, out_extension='csv')
+
+    Ppo.clear_rows_contain_null(mf_file_names[0], null_word='NULL')
+
+    return mf_file_names[len(merged_file_processes) - 1]
 
 
 def main():
-    repo_dir = 'Datasets/after_login_datasets/'  # Directory of dataset files
-    # process_page_views(repo_dir)
-    process_video_views(repo_dir)
+    ds_dir = 'datasets/after_login_datasets/'  # Directory of after-login dataset files.
+    video_views_dir = ds_dir + 'video_views_dir/'  # Directory of video-views files and its processed versions.
+    page_views_dir = ds_dir + 'page_views_dir/'  # Directory of page-views files and its processed versions.
+    merged_file_dir = ds_dir + 'merged_file_dir/'  # Directory of merged files and its processed versions.
 
-    print('Success')
+    page_views_data_set = 'pageViews.txt'
+    video_views_data_set = 'videoViews.txt'
+    processed_pv = process_page_views(repo_dir=page_views_dir, pv_data_set=page_views_data_set)
+    processed_vv = process_video_views(repo_dir=video_views_dir, vv_data_set=video_views_data_set)
+
+    merged_file_data_set = Ppo.merge_video_page_views(merged_file_dir, processed_pv, processed_vv, uid_idx=0, sid_idx=1)
+    processed_mf = process_merged_file(repo_dir=merged_file_dir, mf_data_set=merged_file_data_set)
+
+    print('Final pageViews:%s\nFinal videoViews:%s\nFinal mergedFile:%s' % (processed_pv, processed_vv, processed_mf))
+    print('Program has successfully terminated.')
 
 
 if __name__ == '__main__':
     main()
-    exit(0)
--- before_login_preprocessing.py
+++ before_login_preprocessing.py
@@ -1,53 +1,47 @@
-# ######### PROCESS LIST ############################################################
-# CSV: CSV format of input log
-# NoBots: Crawler that visited the page have been removed.
-# SF: Given features are selected and others have been omitted.
-# UserIDs: User ids are extracted according to their IP and User-Agent info.
-# Sorted: Data is sorted by 3 attribute respectively.
-# SessionIDs: Session ids are extracted according to delta time between logs that has same User-ID.
-# AllPages: Logs contains image, redirected links and non-spages have been omitted.
-# FilteredPV: Final summarized page views table with added session-durations, unique-link-counts,link-counts
-# ###################################################################################
+"""
+########################## List of Process Tags  ################################
+-> [CSV]: CSV format of input log
+-> [NoBots]: Crawler that visited the page have been removed.
+-> [SF]: Given features are selected and others have been omitted.
+-> [UserIDs]: User ids are extracted according TiOp their IP and User-Agent info.
+-> [Sorted]: Data is sorted by 3 attribute respectively.
+-> [SessionIDs]: Session ids are extracted according TiOp delta time between logs that has same User-ID.
+-> [AllPages]: Logs contains image, redirected links and non-spages have been omitted.
+-> [FilteredPV]: Final summarized page views table with added session-durations, unique-link-counts,link-counts
+#################################################################################
+"""
 
-import preprocess_operations
+from preprocess_operations import PreProcessOperations as Ppo
 
 
-def create_file_names(repo_dir, ds_name, process_labels, extension):
-    file_names = []
-    cum_p_labels = ['[%s]' % process_labels[0]]  # Cumulative Process Labels
-    for i in range(1, len(process_labels)):
-        cum_p_labels.append('%s[%s]' % (cum_p_labels[i - 1], process_labels[i]))
+def process_page_views(repo_dir, pv_data_set):
+    page_views_processes = ['CSV', 'NoBots', 'SF', 'UserIDs', 'Sorted', 'AllPages', 'SessionIDs', 'FilteredPV']
+    pv_file_names = Ppo.create_file_names_with_p_tags(repo_dir, pv_data_set, page_views_processes, out_extension='csv')
 
-    for label in cum_p_labels:
-        file_names.append(repo_dir + ds_name + label + extension)
-
-    return file_names
+    Ppo.before_login_log_to_csv(pv_file_names[0])
+    Ppo.clean_bots_from_csv(pv_file_names[1], user_agent_idx=9)
+    Ppo.select_features_in_csv(pv_file_names[2], selected_features=[0, 1, 4, 8, 9, 10])
+    users_dict = Ppo.extract_usr_ids(pv_file_names[3], ip_idx=3, usr_agent_idx=4)
+    Ppo.write_usr_ids(pv_file_names[3], users_dict, ip_idx=3, usr_agent_idx=4)
+    Ppo.sort_csv_by_header(pv_file_names[4], feature1='user_id', feature2='date', feature3='time', feature4=None)
+    Ppo.clean_non_page_links_from_csv(pv_file_names[5], uid_idx=0, date_idx=1, time_idx=2, url_idx=3)
+    Ppo.calc_session_ids(pv_file_names[6], user_id_idx=0, date_idx=1, time_idx=2, time_format='%H:%M:%S',
+                         date_format='%Y-%m-%d')
+    Ppo.filter_page_views(pv_file_names[7], usr_idx=1, sid_idx=0, date_idx=2, time_idx=3, url_idx=4,
+                          time_format='%H:%M:%S', date_format='%Y-%m-%d')
+    return pv_file_names[len(page_views_processes)]
 
 
 def main():
-    repo_dir = 'Datasets/before_login_datasets/'  # Directory of dataset files
+    ds_dir = 'datasets/before_login_datasets/'  # Directory of dataset files
+    page_views_dir = ds_dir + 'page_views_dir/'
+    page_views_data_set = 'u_extend15.log'  # name of log data-set.
 
-    process_labels = ['CSV', 'NoBots', 'SF', 'UserIDs', 'Sorted', 'AllPages', 'SessionIDs', 'FilteredPV']
-    data_set = 'u_extend15'  # name of data set
+    processed_pv = process_page_views(repo_dir=page_views_dir, pv_data_set=page_views_data_set)
 
-    file_names = create_file_names(repo_dir, data_set, process_labels, extension='.csv')
-    ppo = preprocess_operations.PreProcessOperations()
-
-    ppo.raw_log_to_csv(repo_dir + data_set + '.log')
-    ppo.clean_bots_from_csv(file_names[0], user_agent_idx=9)
-    ppo.select_features_in_csv(file_names[1], selected_features=[0, 1, 4, 8, 9, 10])
-    users_dict = ppo.extract_usr_ids(file_names[2], ip_idx=3, usr_agent_idx=4)
-    ppo.write_usr_ids(file_names[2], users_dict, ip_idx=3, usr_agent_idx=4)
-    ppo.sort_csv_by_header(file_names[3], feature1='user_id', feature2='date', feature3='time', feature4=None)
-    ppo.clean_non_page_links_from_csv(file_names[4], uid_idx=0, date_idx=1, time_idx=2, url_idx=3)
-    ppo.calc_session_ids(file_names[5], user_id_idx=0, date_idx=1, time_idx=2, time_format='%H:%M:%S',
-                         date_format='%Y-%m-%d')
-    ppo.filter_page_views(file_names[6], usr_idx=1, sid_idx=0, date_idx=2, time_idx=3, url_idx=4,
-                          time_format='%H:%M:%S', date_format='%Y-%m-%d')
-
-    print('Success')
+    print('Final pageViews:%s' % processed_pv)
+    print('Program has successfully terminated.')
 
 
 if __name__ == '__main__':
     main()
-    exit(0)
